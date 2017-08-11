be067ab2047433e0c377905137fc7521d1e5389a
johnrobert78 <30653569+johnrobert78@users.noreply.github.com>
2017-08-11 17:37:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/first-page', function (req, res) {
+  res.sendFile('page one requested itll serve here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
